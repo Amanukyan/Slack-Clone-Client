7a953a3c5811b9c3a12380d3cdfeb2b4f7ab800f
alex <amanukya@student.42.fr>
2019-05-20 16:09:21 -0700

    Register Form in React Using Semantic UI and GraphQL

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import 'semantic-ui-css/semantic.min.css';
 
 import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
