c40aba5466f5537c3eedc04af994942f836757cd
alex <amanukya@student.42.fr>
2019-05-23 13:13:53 -0700

    Sharing Data Across React Components by Lifting A GraphQL Query

--- src/containers/SideBar.js
+++ src/containers/SideBar.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
-import findIndex from 'lodash/findIndex';
 import decode from 'jwt-decode';
 
 import Channels from '../components/Channels';
 import Teams from '../components/Teams';
 import AddChannelModal from '../components/AddChannelModal';
-import { allTeamsQuery } from '../graphql/team';
 
-class Sidebar extends React.Component {
+export default class Sidebar extends React.Component {
   state = {
     openAddChannelModal: false,
   };
@@ -22,18 +19,7 @@ class Sidebar extends React.Component {
   };
 
   render() {
-    const {
-      data: { loading, allTeams },
-      currentTeamId,
-    } = this.props;
-    if (loading) {
-      return null;
-    }
-
-    const teamIdx = currentTeamId
-      ? findIndex(allTeams, ['id', parseInt(currentTeamId, 10)])
-      : 0;
-    const team = allTeams[teamIdx];
+    const { teams, team } = this.props;
 
     let username = '';
     try {
@@ -44,13 +30,7 @@ class Sidebar extends React.Component {
     } catch (err) {}
 
     return [
-      <Teams
-        key="team-sidebar"
-        teams={allTeams.map((t) => ({
-          id: t.id,
-          letter: t.name.charAt(0).toUpperCase(),
-        }))}
-      />,
+      <Teams key="team-sidebar" teams={teams} />,
       <Channels
         key="channels-sidebar"
         teamName={team.name}
@@ -69,5 +49,3 @@ class Sidebar extends React.Component {
     ];
   }
 }
-
-export default graphql(allTeamsQuery)(Sidebar);
