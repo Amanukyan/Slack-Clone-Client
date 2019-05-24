c066640dd37e76558459a183993e1939d7205d82
alex <amanukya@student.42.fr>
2019-05-24 15:34:49 -0700

    GraphQL Subscriptions Setup

--- src/index.js
+++ src/index.js
@@ -1,60 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from 'react-apollo';
 import 'semantic-ui-css/semantic.min.css';
 
 import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
-
-const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' });
-
-const middlewareLink = new ApolloLink((operation, forward) => {
-  // add the authorization to the headers
-  operation.setContext(({ headers = {} }) => ({
-    headers: {
-      ...headers,
-      'x-token': localStorage.getItem('token') || null,
-      'x-refresh-token': localStorage.getItem('refreshToken') || null,
-    },
-  }));
-
-  return forward(operation);
-});
-
-const afterwareLink = new ApolloLink((operation, forward) =>
-  forward(operation).map((response) => {
-    const {
-      response: { headers },
-    } = operation.getContext();
-    if (headers) {
-      const token = headers.get('x-token');
-      const refreshToken = headers.get('x-refresh-token');
-
-      if (token) {
-        localStorage.setItem('token', token);
-      }
-
-      if (refreshToken) {
-        localStorage.setItem('refreshToken', refreshToken);
-      }
-    }
-
-    return response;
-  }),
-);
-
-const httpLinkWithMiddleware = afterwareLink.concat(
-  middlewareLink.concat(httpLink),
-);
-
-const client = new ApolloClient({
-  link: httpLinkWithMiddleware,
-  cache: new InMemoryCache(),
-});
+import client from './apollo';
 
 const App = (
   <ApolloProvider client={client}>
