caad58a52f8ff895faa9cc75dfc3674b67b90c2d
alex <amanukya@student.42.fr>
2019-05-24 18:15:51 -0700

    Unsubscribing from GraphQL Subscriptions

---
+++ src/apollo.js
@@ -0,0 +1,68 @@
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink, split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' });
+
+const middlewareLink = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      'x-token': localStorage.getItem('token') || null,
+      'x-refresh-token': localStorage.getItem('refreshToken') || null,
+    },
+  }));
+
+  return forward(operation);
+});
+
+const afterwareLink = new ApolloLink((operation, forward) =>
+  forward(operation).map((response) => {
+    const {
+      response: { headers },
+    } = operation.getContext();
+    if (headers) {
+      const token = headers.get('x-token');
+      const refreshToken = headers.get('x-refresh-token');
+
+      if (token) {
+        localStorage.setItem('token', token);
+      }
+
+      if (refreshToken) {
+        localStorage.setItem('refreshToken', refreshToken);
+      }
+    }
+
+    return response;
+  }),
+);
+
+const httpLinkWithMiddleware = afterwareLink.concat(
+  middlewareLink.concat(httpLink),
+);
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000/subscriptions',
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLinkWithMiddleware,
+);
+
+export default new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
